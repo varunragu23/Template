83fcff9bfad734da33090dd64db8454948871dd0
Ragunath Devarasu <ragunathd@gmail.com>
2019-03-12 16:16:47 -0700

    template

---
+++ cpp/a.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define watch(x) cout << (#x) << " is " << (x) << endl
+
+int main(int argc, char const *argv[])
+{
+
+  freopen("task.in", "r", stdin);   // redirects standard input
+  freopen("task.out", "w", stdout); // redirects standard output
+
+  // To speed input/output
+  cin.sync_with_stdio(0);
+  cin.tie(0);
+
+  // use cin and cout from this point onwards.
+
+  int N, M;
+
+  cin >> N >> M;
+
+  cout << N << " " << M << endl;
+  return 0;
+}
