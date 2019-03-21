1c4c6cb7d973075374b4314c964893d598dc94ad
Ragunath Devarasu <ragunathd@gmail.com>
2019-03-20 20:42:23 -0700

    init 2

--- cpp/a.cpp
+++ cpp/a.cpp
@@ -1,5 +1,28 @@
 #include <bits/stdc++.h>
-
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define all(cont) cont.begin(), cont.end()
+#define rall(cont) cont.end(), cont.begin()
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define IN(A, B, C) assert( B <= A && A <= C)
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+const double pi=acos(-1.0);
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
 using namespace std;
 
 #define watch(x) cout << (#x) << " is " << (x) << endl
