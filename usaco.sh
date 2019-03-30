88939ca2e231b9a21dfa222ce659bebd4791a58c
Ragunath Devarasu <raguanthd@gmail.com>
2019-03-30 09:03:15 -0700

    init 15

--- usaco.sh
+++ usaco.sh
@@ -26,13 +26,19 @@ echo "Creating $LANG project in $PROJECT_NAME directory"
 
 rm -rf ./$PROJECT_NAME
 
-cp -Rf /Users/ragunathd/Code/Template/$LANG ./$PROJECT_NAME
+cp -Rf $HOME/Code/Template/$LANG ./$PROJECT_NAME
 mv ./$PROJECT_NAME/task.in ./$PROJECT_NAME/${PROJECT_NAME}.in
 mv ./$PROJECT_NAME/task.out ./$PROJECT_NAME/${PROJECT_NAME}.out
-if [ -f "${PROJECT_NAME}/a.cpp" ]; then
-  sed -i '' "s/task/${PROJECT_NAME}/g" "$PROJECT_NAME/a.cpp"
+mv ./$PROJECT_NAME/a.cpp ./$PROJECT_NAME/a.bak.cpp
+if [ -f "${PROJECT_NAME}/a1.cpp" ]; then
+  sed -i '' "s/task/${PROJECT_NAME}/g" "$PROJECT_NAME/a1.cpp"
+fi
+if [ -f "${PROJECT_NAME}/a2.cpp" ]; then
+  sed -i '' "s/task/${PROJECT_NAME}/g" "$PROJECT_NAME/a2.cpp"
 fi
 if [ -f "${PROJECT_NAME}/Task.java" ]; then
   sed -i '' "s/task/${PROJECT_NAME}/g" "$PROJECT_NAME/Task.java"
 fi
+
+code $PROJECT_NAME
 # -----------------------------------------------------------------
