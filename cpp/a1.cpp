fdfe4cfebc2598466662e76740fd9b5a8d8ecb6c
Ragunath Devarasu <ragunathd@ragu-mac.local>
2019-09-10 18:24:13 -0700

    added templates

--- cpp/a1.cpp
+++ cpp/a1.cpp
@@ -70,6 +70,48 @@ typedef vector<string> VS;
   for_each(v.begin(), v.end(), [](int i) -> void { cerr << i << ","; }); \
   cerr << endl;
 
+/// ********* debug template by Bidhan Roy *********
+
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+    os << "{";
+    typename vector< T > :: const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "}";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+    os << "[";
+    typename set< T > :: const_iterator it;
+    for ( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+    os << "[";
+    typename map< F , S >::const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << it -> first << " = " << it -> second ;
+    }
+    return os << "]";
+}
+
+#define deb(x) cerr << #x << " = " << x << endl;
+
 int main(int argc, char const *argv[]) {
 #ifndef ONLINE_JUDGE
   freopen("task.in", "r", stdin);    
