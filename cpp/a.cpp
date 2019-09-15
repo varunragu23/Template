a4f46e09f90c16be81c9a820262cb14754c018c3
Ragunath Devarasu <ragunathd@ragu-mac.local>
2019-09-15 14:40:06 -0700

    v3

--- cpp/a.cpp
+++ cpp/a.cpp
@@ -4,98 +4,49 @@ LANG: C++
 TASK: task
 */
 #pragma region Headers, define, typedef
-#include <assert.h>
-#include <limits.h>
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#include <algorithm>
-#include <bitset>
-#include <deque>
-#include <functional>
-#include <iostream>
-#include <iterator>
-#include <limits>
-#include <list>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
-#include <string>
-#include <utility>
-#include <vector>
+#include<bits/stdc++.h>
 
 using namespace std;
+#ifdef DEBUG
+#include "debug.h"
+#endif
 
-#define MEM(a, b) memset(a, (b), sizeof(a))
-#define all(cont) cont.begin(), cont.end()
-#define rall(cont) cont.end(), cont.begin()
-#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define EPS 1e-9
 #define IN(A, B, C) assert(B <= A && A <= C)
+#define INF (int)1e9
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define MOD 1000000007
 #define MP make_pair
 #define PB push_back
-#define INF (int)1e9
-#define EPS 1e-9
-#define PI 3.1415926535897932384626433832795
-#define MOD 1000000007
+#define all(cont) cont.begin(), cont.end()
+#define rall(cont) cont.end(), cont.begin()
 
-const double pi = acos(-1.0);
+const double PI = acos(-1.0);
+typedef long long ll;
+typedef long double ld;
 typedef pair<int, int> PII;
-typedef vector<int> VI;
-typedef vector<string> VS;
-typedef vector<PII> VII;
-typedef vector<VI> VVI;
 typedef map<int, int> MPII;
+typedef multiset<int> MSETI;
 typedef set<int> SETI;
 typedef set<string> SETS;
-typedef multiset<int> MSETI;
-typedef long int int32;
-typedef unsigned long int uint32;
-typedef long long int int64;
-typedef unsigned long long int uint64;
+typedef vector<int> VI;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
 
-template<class T> ostream &operator<<(ostream& os, vector<T> V) {
-  os << "["; for (auto vv : V) os << vv << ","; return os << "]";
-}
-template<class L, class R> ostream &operator<<(ostream &os, pair<L,R> P) {
-  return os << "(" << P.st << "," << P.nd << ")";
-}
+#define deb(x) cerr << #x << " = " << x << endl;
 
-#define watch(x) cerr << (#x) << " is " << (x) << endl
-#define printvii(v)                                    \
-  cerr << (#v) << "=";                                 \
-  for_each(v.begin(), v.end(), [](PII i) -> void {     \
-    cerr << "(" << i.first << "," << i.second << "),"; \
-  });                                                  \
-  cerr << endl;
-#define printvi(v)                                                       \
-  cerr << (#v) << "=";                                                   \
-  for_each(v.begin(), v.end(), [](int i) -> void { cerr << i << ","; }); \
-  cerr << endl;
+// util functions
+
 #pragma endregion
+
 
 int main(int argc, char const *argv[]) {
 #ifndef ONLINE_JUDGE
-  freopen("task.in", "r", stdin);    // redirects standard input
-  freopen("task.out", "w", stdout);  // redirects standard output
+  freopen("task.in", "r", stdin);    
+  freopen("task.out", "w", stdout); 
 #endif
+  cin.sync_with_stdio(0); cin.tie(0);
 
-  // To speed input/output
-  cin.sync_with_stdio(0);
-  cin.tie(0);
-
-  // use cin and cout from this point onwards.
-  int N, M;
-
-  for (int i = 0; i < 5; i++) {
-  }
-
-  cin >> N >> M;
-
-  cout << N << " " << M << endl;
   return 0;
 }
--- usaco.sh
+++ usaco.sh
@@ -28,17 +28,15 @@ rm -rf ./$PROJECT_NAME
 
 cp -Rf $HOME/Code/Template/$LANG ./$PROJECT_NAME
 mv ./$PROJECT_NAME/task.in ./$PROJECT_NAME/${PROJECT_NAME}.in
-mv ./$PROJECT_NAME/task.out ./$PROJECT_NAME/${PROJECT_NAME}.out
-mv ./$PROJECT_NAME/a.cpp ./$PROJECT_NAME/a.bak.cpp
-if [ -f "${PROJECT_NAME}/a1.cpp" ]; then
-  sed -i '' "s/task/${PROJECT_NAME}/g" "$PROJECT_NAME/a1.cpp"
-fi
-if [ -f "${PROJECT_NAME}/a2.cpp" ]; then
-  sed -i '' "s/task/${PROJECT_NAME}/g" "$PROJECT_NAME/a2.cpp"
+if [ -f "${PROJECT_NAME}/a.cpp" ]; then
+  sed -i '' "s/task/${PROJECT_NAME}/g" "$PROJECT_NAME/a.cpp"
 fi
 if [ -f "${PROJECT_NAME}/Task.java" ]; then
   sed -i '' "s/task/${PROJECT_NAME}/g" "$PROJECT_NAME/Task.java"
 fi
 
-code $PROJECT_NAME
+cd $PROJECT_NAME
+git init
+
+code .
 # -----------------------------------------------------------------
