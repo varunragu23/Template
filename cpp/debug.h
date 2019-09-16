dceaeb97cfe647ec067ee5a1c2b5c3b606404058
Ragunath Devarasu <raguanthd@gmail.com>
2019-09-15 21:12:57 -0700

    v7

--- cpp/debug.h
+++ cpp/debug.h
@@ -35,3 +35,12 @@ ostream &operator << ( ostream & os, const map< F, S > &v ) {
     }
     return os << "]";
 }
+
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+cerr << *it << " = " << a << endl;
+err(++it, args...);
+}
