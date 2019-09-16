c093ce88ca9f820594acf13dbc190e116cb6527d
Ragunath Devarasu <ragunathd@gmail.com>
2019-09-16 11:54:19 -0700

    updated perimission

--- cf.sh
+++ cf.sh
@@ -4,7 +4,7 @@
 #          Description
 # ------------------------------------------------------------------
 
-VERSION=2.0
+VERSION=3.0
 SUBJECT=Create-CF-Projects
 USAGE="Usage: cf.sh <contestid> <problemid>"
 LANG=cpp
