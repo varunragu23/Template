0f85b2e2fbe49dba45fb83848515a8f1ad4d82fe
Ragunath Devarasu <ragunathd@gmail.com>
2019-09-21 21:03:39 -0700

    all

--- cleanup.sh
+++ cleanup.sh
@@ -5,5 +5,5 @@ find . -type f -name '*.class' -delete
 find . -type f -name 'a1' -delete
 find . -type f -name 'a2' -delete
 find . -type d -name '*.dSYM' | xargs rm -r
+find ~/Code/Varun -type d -name '.git' | xargs rm -r
 find . -type d -name 'ipch' | xargs rm -r
-
