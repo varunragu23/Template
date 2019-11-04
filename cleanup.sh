dbddf46135dc897f81aedb5384d90f075fff5798
Ragunath Devarasu <ragunathd@gmail.com>
2019-11-03 17:36:53 -0800

    update cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -6,11 +6,11 @@ find . -type f -name 'a1' -delete
 find . -type f -name 'a2' -delete
 find . -type d -name '*.dSYM' | xargs rm -r
 find ~/Code/Varun -type d -name '.git' | xargs rm -r
-find ~/Code/OldVarun -type d -name '.git' | xargs rm -r
-find ~/Code/OldVarun -type d -name '.vscode' | xargs rm -r
-find ~/Code/OldVarun -type f -name '.gitignore' | xargs rm -r
-find ~/Code/OldVarun -type f -name 'tasks.json' | xargs rm -r
-find ~/Code/OldVarun -type f -name 'settings.json' | xargs rm -r
-find ~/Code/OldVarun -type f -name 'launch.json' | xargs rm -r
-find ~/Code/OldVarun -type f -name 'c_cpp_properties.json' | xargs rm -r
+#find ~/Code/OldVarun -type d -name '.git' | xargs rm -r
+#find ~/Code/OldVarun -type d -name '.vscode' | xargs rm -r
+#find ~/Code/OldVarun -type f -name '.gitignore' | xargs rm -r
+#find ~/Code/OldVarun -type f -name 'tasks.json' | xargs rm -r
+#find ~/Code/OldVarun -type f -name 'settings.json' | xargs rm -r
+#find ~/Code/OldVarun -type f -name 'launch.json' | xargs rm -r
+#find ~/Code/OldVarun -type f -name 'c_cpp_properties.json' | xargs rm -r
 find . -type d -name 'ipch' | xargs rm -r
