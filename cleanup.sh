c8189791bc93a3d97c658bffd08dd4bbaf1d9364
Varun Ragunath <varunragu23@gmail.com>
2019-12-10 16:11:22 -0800

    b.cpp support

--- cleanup.sh
+++ cleanup.sh
@@ -1,9 +1,8 @@
 find . -type f -name 'a.out' -delete
 find . -type f -name 'a' -delete
+find . -type f -name 'b' -delete
 find . -type f -name '*.exe' -delete
 find . -type f -name '*.class' -delete
-find . -type f -name 'a1' -delete
-find . -type f -name 'a2' -delete
 find . -type d -name '*.dSYM' | xargs rm -r
 find ~/Code/Varun -type d -name '.git' | xargs rm -r
 #find ~/Code/OldVarun -type d -name '.git' | xargs rm -r
