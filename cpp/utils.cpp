0373380b067b4cae52b1ca61ba33cb1b6297b442
Varun Ragunath <varunragu23@gmail.com>
2020-03-13 22:33:52 -0700

    fast io

--- cpp/utils.cpp
+++ cpp/utils.cpp
@@ -61,3 +61,35 @@ void out(int x) {
 	while(lz--)
 		putchar_unlocked('0');
 }
+
+// TO forcefully inline a function
+// __attribute__((always_inline)) <function definition>
+//
+
+//  Arithmetic operation performance is ordered roughly (SLOW to FAST): transcendental  functions, square root, modulo, divide, multiply, add/subtract/mutiply by  power of 2/divide by power of 2/modulo by a power of 2
+
+/*
+ * SPEED UP TRICKS
+Use prefix(++i) instead of postfix(i++) increment/decrement, as postfix uses a temporary variable to save the initial state of the variable.
+Use short hand assignment operators (+=, -=), they don’t create a temporary variable.
+Use bit-wise operators where ever it is possible. Operations on bits is always more efficient than directly on decimal. Using x >> 1 instead of x / 2.
+Use static memory allocation instead of dynamic memory allocation. Various data structures like Trees can be implemented by both array and pointers. Array implementation is far more efficient as compared to pointer implementation.
+Make the number of arguments passed to a function as less as possible.
+Make your functions inline.
+Iteration is always better than recursion performance wise.
+Passing variable by reference is more efficient, because no copying of variable takes place.
+Use memoisation, if same value is calculated again and again.
+Use ‘\n’ instead of `endl` operator. `endl` forces data stream to be flushed.
+Use scanf()/prinf() instead of cin/cout objects.
+Last one, adding unnecessary header files takes much compilation time. After all programmers time is also important. So, don’t use #include <bits/stdc++.h> everywhere apart from competitions.
+
+Modular and divison are slow;
+
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("O3")
+#pragma GCC target("sse4")
+
+*/
+
+// MyCompetitiveProgramming/zz-algorthmic-resources/tutorial-links.txt
+// CompetitiveProgramming/Index_of_Cancerous_Codes.md
