8c219f2595bb0e3ab98cf3610ee35e6602bc7752
Varun Ragunath <varunragu23@gmail.com>
2020-12-21 11:50:05 -0800

    modified gen

--- testing/test.sh
+++ testing/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SUBJECT="Gen tests, find output using brute and check against progName"
-USAGE="Usage: ./tests.sh TestCount progName"
+USAGE="Usage: ./tests.sh TestCount progName parallelCount"
 # --- Options processing -------------------------------------------
 if [ $# -le 1 ] ; then
     echo $USAGE
@@ -9,6 +9,7 @@ fi
 
 tCount=$1
 progName=$2
+parallelCount=$3
 mkdir -p data
 
 if [ -z "$tCount" ]
@@ -19,6 +20,10 @@ if [ -z "$progName" ]
 then
 	progName=a
 fi
+if [ -z "$parallelCount" ]
+then
+	parallelCount=6
+fi
 rm -rf data/failed.*
 rm -rf data/inp*
 rm -rf data/oac*
@@ -27,14 +32,15 @@ failed=0
 for((i = 1; i <= $tCount; ++i)); do
     name=`printf "%02d" $i`
     echo "==== test $name ===="
-    for((j = 1; j <= 8; ++j)); do
+    for((j = 1; j <= $parallelCount; ++j)); do
         gtime -f "$i.$j gen %e sec, %M KB" ./gen $i$j > data/inp$j
         echo "$i.$j `head -1 data/inp$j`"
         gtime -f "$i.$j brute %e sec, %M KB" ./brute < data/inp$j > data/oac$j &
+        sleep 0.1
     done
     echo "spawned brute. waiting for completion"
     wait
-    for((j = 1; j <= 8; ++j)); do
+    for((j = 1; j <= $parallelCount; ++j)); do
         gtime -f "$i.$j $progName %e sec, %M KB" ./$progName < data/inp$j > data/out$j
         diff -w data/out$j data/oac$j 
         if [ "$?" != "0" ]
@@ -46,6 +52,9 @@ for((i = 1; i <= $tCount; ++i)); do
         fi
     done
 done
+rm -rf data/inp*
+rm -rf data/oac*
+rm -rf data/out*
 if [ "$failed" != "0" ]
 then
     echo "Failed $failed Tests"
